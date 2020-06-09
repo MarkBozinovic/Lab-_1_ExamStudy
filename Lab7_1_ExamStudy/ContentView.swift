b7d9110b732c9d199546ea1de9f55496518d64fe
Mark Bozinovic <marko.bozinovic@live.vu.edu.au>
2020-06-09 18:13:23 +1000

    Add files via upload

---
+++ Lab7_1_ExamStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab7_1_ExamStudy
+//
+//  Created by user169740 on 6/9/20.
+//  Copyright © 2020 user169740. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
